512c620fd8ab42a4cb64a352c5a0adeadd244629
henriAlex13 <103936640+henriAlex13@users.noreply.github.com>
2026-07-16 15:02:48 +0000

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -8,3 +8,11 @@ if 'NUM_BORDEREAU' in df_central.columns:
     st.write("Tensions disponibles:", df_central['TENSION'].unique())
     df_debug = df_central[df_central['DATE'] == periode_bt][['IDENTIFIANT', 'DATE', 'TENSION', 'NUM_BORDEREAU']]
     st.write("Lignes pour cette période:", df_debug)
+
+
+
+st.write("Colonnes df_bt_cumul:", list(df_bt_cumul.columns))
+st.write("Config num_bord_col:", CONFIG_BT['num_bord_col'])
+st.write("Colonne présente:", CONFIG_BT['num_bord_col'] in df_bt_cumul.columns)
+if CONFIG_BT['num_bord_col'] in df_bt_cumul.columns:
+    st.write("Valeurs:", df_bt_cumul[CONFIG_BT['num_bord_col']].unique())
